2aa755d7407074542da8589ec902f5dff017b957
Dmitriy <d.metelkov@mail.ru>
2018-03-27 12:52:59 +0400

    Обновил скрипт попапов

--- js/script.js
+++ js/script.js
@@ -1,10 +1,59 @@
-var activeMap = document.getElementsByClassName('main-contacts__map');
-var popupMap = document.getElementsByClassName('popup-map');
-var closeBtn = document.getElementsByClassName('popup-close');
+var popupWriteLink = document.querySelector(".main-contacts__btn"),
+    popupWrite = document.querySelector(".popup-write-us"),
+    userName = popupWrite.querySelector("[name=user-name]"),
+    userEmail = popupWrite.querySelector("[name=user-email]"),
+    popupWriteForm = popupWrite.querySelector("form"),
+    popupMapLink = document.querySelector(".main-contacts__map"),
+    popupMap = document.querySelector(".popup-map"),
+    popupWriteClose = document.querySelector(".popup-write-us__close"),
+    popupMapClose = document.querySelector(".popup-map__close");
 
-activeMap[0].onclick = function() {
-  popupMap[0].classList.add("popup-map--active");
-}
-closeBtn[1].onclick = function() {
-  popupMap[0].classList.remove("popup-map--active");
-}
+popupWriteLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWrite.classList.add("popup-write-us--active");
+  userName.focus();
+})
+
+popupWriteForm.addEventListener("submit", function (evt) {
+  if (!userName.value || !userEmail.value) {
+    evt.preventDefault();
+    popupWrite.classList.add("popup-write-us--error");
+  }
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupWrite.classList.contains("popup-write-us--active")) {
+      popupWrite.classList.remove("popup-write-us--active");
+      popupWrite.classList.remove("popup-write-us--error");
+    }
+  }
+})
+
+popupWriteClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWrite.classList.remove("popup-write-us--active");
+  popupWrite.classList.remove("popup-write-us--error");
+})
+
+popupMapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.add("popup-map--active");
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupMap.classList.contains("popup-map--active")) {
+      popupMap.classList.remove("popup-map--active");
+    }
+  }
+})
+
+popupMapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("popup-map--active");
+})
+
+
